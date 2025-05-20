c1356e65e3fe68579b4e41b8b10a055fda08011c
QuantumEden <bcousins@alumni.fullsail.edu>
2025-05-20 18:18:03 -0500

    Create __init__.py

---
+++ api/__init__.py
@@ -0,0 +1,10 @@
+# api/__init__.py
+# Eden Protocol – API Package Initialization
+
+"""
+This module initializes the Eden Protocol API package.
+"""
+
+__version__ = "1.0.0"
+__author__ = "QuantumEden"
+__symbol__ = "🜂🜁🜃🜄⚛︎"
